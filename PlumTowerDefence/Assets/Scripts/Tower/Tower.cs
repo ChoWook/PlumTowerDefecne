d910d5123c49defcf857b75d7d31f98492f02ae3
yoon-H <ucando0320@gmail.com>
2022-08-23 18:25:36 +0900

    commit for pull

---
+++ PlumTowerDefence/Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    /*
+     할 일
+    - 
+     
+     
+     
+     
+     
+     */
+    // 변수 : 속성, 공격력, 사거리, 크기, 타워 가격, 업그레이드 단계, 공격 종류(기본, 범위, 디버프 등), 이동 가격
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
