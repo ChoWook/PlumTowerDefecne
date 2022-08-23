dde2b107c7678d7eebc19605a4615200184ee4fa
yoon-H <ucando0320@gmail.com>
2022-08-23 23:36:04 +0900

    한윤재 Add Tower Variables, Make Targeting function
    타워 스텟 변수를 추가하고 적을 gameobject 의 tag "Enemy"로 찾아 타겟팅하게 만들었음. 추적방식은 후에 수정가능하고 테스트는 합치고 진행

--- PlumTowerDefence/Assets/Scripts/Tower/Tower.cs
+++ PlumTowerDefence/Assets/Scripts/Tower/Tower.cs
@@ -10,22 +10,81 @@ public class Tower : MonoBehaviour
      - 속성
      - 
      
-     
+    2. 타깃 조정
      
      */
+
     // 변수 : 속성, 공격력, 사거리, 크기, 타워 가격, 업그레이드 단계, 공격 종류(기본, 범위, 디버프 등), 이동 가격
 
-    
+
+    // 타워 스텟
+
+    private int TowerID;                            // 타워 ID (데이터 테이블)
+    public string TowerName;                        // 타워 이름
+    private int TypeID;                             // 속성 ID (데이터테이블)
+    private int SizeID;                             // 타워 크기 (데이터테이블)
+    private int AttackStat ;                        // 공격력 스텟(데이터테이블)
+    private int SpeedStat ;                         // 공격 속도 스텟(데이터테이블)
+    public float range = 15f;                            // 공격 사거리
+
+    // 적 스텟 (타겟 지정)
+
+    private Transform target;
+
+    public string enemyTag = "Enemy";
+
+    // todo 적 변수 가져 오기
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("UpdateTarget", 0, 0.5f); // 0.5초 마다 반복하기
+    }
+
+    // 타겟 업데이트
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag); // Enemy  태그로 적 찾기
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position); // 적과의 거리 구하기
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
         
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+        else
+        {
+            target = null;
+        }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+
 }
