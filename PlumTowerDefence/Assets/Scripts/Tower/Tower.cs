651a1f9da2c3af7258ad71d471e0598413976989
yoon-H <ucando0320@gmail.com>
2022-08-24 20:47:59 +0900

    commit for pull

--- PlumTowerDefence/Assets/Scripts/Tower/Tower.cs
+++ PlumTowerDefence/Assets/Scripts/Tower/Tower.cs
@@ -17,21 +17,28 @@ public class Tower : MonoBehaviour
     // 변수 : 속성, 공격력, 사거리, 크기, 타워 가격, 업그레이드 단계, 공격 종류(기본, 범위, 디버프 등), 이동 가격
 
 
-    // 타워 스텟
+    // 타워 스텟 (임시로 화살타워 스텟 설정)
 
-    private int TowerID;                            // 타워 ID (데이터 테이블)
-    public string TowerName;                        // 타워 이름
-    private int TypeID;                             // 속성 ID (데이터테이블)
-    private int SizeID;                             // 타워 크기 (데이터테이블)
-    private int AttackStat ;                        // 공격력 스텟(데이터테이블)
-    private int SpeedStat ;                         // 공격 속도 스텟(데이터테이블)
-    public float range = 15f;                            // 공격 사거리
+    private int TowerID = 0;                            // 타워 ID (데이터 테이블)
+    public string TowerName = "화살타워";                        // 타워 이름
+    private int AttackPropertyID = 0;                   // 공격 속성(데이터테이블)
+    private int TypeID = 0;                             // 속성 ID (데이터테이블)
+    private int SizeID = 0;                             // 타워 크기 (데이터테이블)
+    private int AttackStat = 25;                       // 공격력 스텟(데이터테이블)
+    private int SpeedStat = 4;                         // 공격 속도 스텟(데이터테이블)
+    private int AbilityStat;                        // 특수 능력 스텟(데이터테이블)
+    public int Range = 3;                       // 공격 사거리
+    private int UpgradePrice = 40;                       // 업그레이드 가격(데이터테이블)
+    private int Price = 100;                              // 구매 가격(데이터테이블)
+    private double Damage;                             // 데미지 
 
     // 적 스텟 (타겟 지정)
 
-    private Transform target;
+    public Transform Target;
 
     public string enemyTag = "Enemy";
+
+    public int DefenceStat = 10;
 
     // todo 적 변수 가져 오기
 
@@ -61,30 +68,55 @@ public class Tower : MonoBehaviour
         
         }
 
-        if (nearestEnemy != null && shortestDistance <= range)
+        if (nearestEnemy != null && shortestDistance <= Range)
         {
-            target = nearestEnemy.transform;
+            Target = nearestEnemy.transform;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target == null)
+        if (Target == null)
         {
             return;
         }
         else
         {
-            target = null;
+            Target = null;
         }
     }
 
+    // 사거리 표시
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.DrawWireSphere(transform.position, Range);
     }
+
+
+    // 타워 공격 속성별 데미지 설정 (적 기획과 조율 필요)
+    private void SetDamageWithAttackProperty()
+    {
+        switch(AttackPropertyID)
+        {
+            case 0:                                // 타워의 공격력 x {0.01x (100 - 적의 방어력)} 만큼 대상에게 데미지를 준다.
+                Damage = AttackStat * (0.01 * (100 - DefenceStat));
+                break;
+            case 1:                               // 체력에 타워의 공격력 x 1.2 x{0.01x(100-방어력)} 만큼 대상에게 데미지를 준다, 방어구에는 기본 알고리즘과 같다
+
+                break;
+            case 2 :
+
+                break;
+
+            case 3 :
+                break;
+                
+
+        }    
+    }
+
 
 
 }
