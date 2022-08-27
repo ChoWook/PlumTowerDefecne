7713d97da4758241e580d78c4f1fdd9d60219b5e
ChoWook <41852462+ChoWook@users.noreply.github.com>
2022-08-28 01:20:21 +0900

    김초욱 애셋 추가 및 자원 애셋 적용
    애셋 추가 및 자원 애셋 적용

--- PlumTowerDefence/Assets/Scripts/Map/Resource.cs
+++ PlumTowerDefence/Assets/Scripts/Map/Resource.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EResourceType ResourceType = EResourceType.Magnetite;
+
+    [SerializeField] GameObject[] Resources;
+
+    float[] Probs;
+
+#if UNITY_EDITOR
+    private void Update()
     {
-        
+        UpdateResourceType();
+    }
+#endif
+
+    private void OnEnable()
+    {
+        Probs = new float[Resources.Length];
+
+        Tables.Load();
+
+        for (int i = 0; i < Probs.Length; i++)
+        {
+            Probs[i] = Tables.MapGimmickResource.Get(i + 1)._Probability;
+        }
+
+        SetResourceType(ChooseType());
     }
 
-    // Update is called once per frame
-    void Update()
+    int ChooseType()
     {
-        
+        float total = 0;
+
+        foreach (float elem in Probs)
+        {
+            total += elem;
+        }
+
+        float randomPoint = Random.Range(0, total);
+
+        for (int i = 0; i < Probs.Length; i++)
+        {
+            if (randomPoint < Probs[i])
+            {
+                return i;
+            }
+            else
+            {
+                randomPoint -= Probs[i];
+            }
+        }
+        return Probs.Length - 1;
+    }
+
+    public void SetResourceType(int Sender)
+    {
+        ResourceType = (EResourceType)(Sender + 1);
+
+        UpdateResourceType();
+    }
+
+    public void UpdateResourceType()
+    {
+        for(int i = 0; i < Resources.Length; i++)
+        {
+            if(Resources[i].name.CompareTo(ResourceType.ToString()) == 0)
+            {
+                Resources[i].SetActive(true);
+            }
+            else
+            {
+                Resources[i].SetActive(false);
+            }
+        }
     }
 }
