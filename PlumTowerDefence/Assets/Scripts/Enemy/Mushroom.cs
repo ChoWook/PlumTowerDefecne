8c4aed29b8d3a066583f39a073b3f212a2775659
조강현 <111622974+LewisCho7@users.noreply.github.com>
2022-08-30 15:42:50 +0900

    조강현 몬스터 종류 구분 소환

---
+++ PlumTowerDefence/Assets/Scripts/Enemy/Mushroom.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : Enemy
+{
+    void GetStat()
+    {
+        BaseHP = Tables.Monster.Get(2)._Hp;
+        BaseShield = Tables.Monster.Get(2)._Sheild;
+        BaseArmor = Tables.Monster.Get(2)._Armor;
+        BaseSpeed = Tables.Monster.Get(2)._Speed;
+        Debug.Log(BaseHP + " " + BaseShield + " " + BaseArmor + " " + BaseSpeed);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlumTowerDefence/Assets/Scripts/Enemy/Spider.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spider : Enemy
+{
+    void GetStat()
+    {
+        BaseHP = Tables.Monster.Get(7)._Hp;
+        BaseShield = Tables.Monster.Get(7)._Sheild;
+        BaseArmor = Tables.Monster.Get(7)._Armor;
+        BaseSpeed = Tables.Monster.Get(7)._Speed;
+        Debug.Log(BaseHP + " " + BaseShield + " " + BaseArmor + " " + BaseSpeed);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
