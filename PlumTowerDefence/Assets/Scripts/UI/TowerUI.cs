b3b55286add36ea266f2561233d1ed6f637e984c
seobok <tjqhrals99@naver.com>
2022-09-02 12:21:07 +0900

    서복민 update TowerUI
    TowerUI 를 만들기 위한 기초작업 밑 createMap 스크립트 생성

--- PlumTowerDefence/Assets/Scripts/UI/TowerUI.cs
+++ PlumTowerDefence/Assets/Scripts/UI/TowerUI.cs
@@ -2,8 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TowerUI : MonoBehaviour
+public class TowerUI : MonoBehaviour, IPointerClickHandler
 {
-    
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        ShowTowerUI();
+    }
+
+    private void ShowTowerUI()
+    {
+        Debug.Log("Clicked");
+    }
 }
