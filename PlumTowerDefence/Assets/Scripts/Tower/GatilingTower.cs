aa8d7eef0555bb2f92d39766108341b0774262ef
yoon-H <ucando0320@gmail.com>
2022-09-04 13:44:22 +0900

    한윤재 GatlingTower

---
+++ PlumTowerDefence/Assets/Scripts/Tower/GatilingTower.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatilingTower : Tower
+{
+    private void Awake()
+    {
+        Setstat(ETowerName.Gatling);
+    }
+}
