fe9c6d734c7d2ca85c13640835744fa7425ec324
yoon-H <ucando0320@gmail.com>
2022-09-07 03:56:29 +0900

    한윤재 poisontower, Hourglasstower 기능 추가

--- PlumTowerDefence/Assets/Scripts/Tower/HourglassTower.cs
+++ PlumTowerDefence/Assets/Scripts/Tower/HourglassTower.cs
@@ -8,4 +8,42 @@ public class HourglassTower : Tower
     {
         Setstat(ETowerName.Hourglass);
     }
+
+    protected override void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy <= RealRange)
+            {
+                EnemyList.Add(enemy);
+            }
+        }
+
+    }
+
+    protected override void Update()
+    {
+        if (EnemyList.Count == 0)
+            return;
+
+        Shoot();
+
+
+    }
+
+
+
+
+    public override void Shoot()
+    {
+        for (int i = 0; i < EnemyList.Count; i++)
+        {
+            EnemyList[i].GetComponent<Enemy>().SlowEnemy(AbilityStat);
+        }
+    }
+
 }
