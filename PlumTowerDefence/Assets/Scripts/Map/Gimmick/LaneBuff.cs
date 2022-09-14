4a80e8e50ac61ed8b80911b622841bac32c375a1
ChoWook <41852462+ChoWook@users.noreply.github.com>
2022-09-14 16:45:21 +0900

    김초욱 공격로 버프 디버프 구분
    공격로 버프 디버프 구분

--- PlumTowerDefence/Assets/Scripts/Map/Gimmick/LaneBuff.cs
+++ PlumTowerDefence/Assets/Scripts/Map/Gimmick/LaneBuff.cs
@@ -5,6 +5,10 @@ using UnityEngine.EventSystems;
 
 public class LaneBuff : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] GameObject BuffEffect;
+
+    [SerializeField] GameObject DeBuffEffect;
+
     public ELaneBuffType Type;
 
 
@@ -33,6 +37,17 @@ public class LaneBuff : MonoBehaviour, IPointerClickHandler
     public void SetType(ELaneBuffType Sender)
     {
         Type = Sender;
+
+        if (Tables.MonsterLaneBuff.Get(Type)._IsBuff)
+        {
+            BuffEffect.SetActive(true);
+            DeBuffEffect.SetActive(false);
+        }
+        else
+        {
+            BuffEffect.SetActive(false);
+            DeBuffEffect.SetActive(true);
+        }
     }
 
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
