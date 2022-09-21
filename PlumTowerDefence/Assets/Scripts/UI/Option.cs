6ad5a0104bda8082ea894f0ec0167050d712ee99
ChoWook <41852462+ChoWook@users.noreply.github.com>
2022-09-21 23:33:07 +0900

    김초욱 환경설정 BGM On/Off 기능 추가
    환경설정 BGM On/Off 기능 추가

--- PlumTowerDefence/Assets/Scripts/UI/Option.cs
+++ PlumTowerDefence/Assets/Scripts/UI/Option.cs
@@ -3,12 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Option : MonoBehaviour
 {
     /// <summary>
     /// option창을 담당하는 스크립트
     /// </summary>
+    /// 
+
+    [SerializeField] Toggle ShowMonsterUIToggle;
+
+    [SerializeField] Toggle MusicSoundToggle;
+
+    [SerializeField] Toggle EffectSoundToggle;
+
+
+    AudioSource BGMSource;
+
+    private void Awake()
+    {
+        BGMSource = Camera.main.GetComponent<AudioSource>();
+    }
+
+    public void OnShowMonterUIToggleValueChanged(bool Checked)
+    {
+
+    }
+
+    public void OnMusicSoundToggleValueChanged(bool Checked)
+    {
+        if (Checked)
+        {
+            BGMSource.Play();
+        }
+        else
+        {
+            BGMSource.Pause();
+        }
+    }
+
+    public void OnEffectSoundToggleValueChanged(bool Checked)
+    {
+
+    }
 
     public void ShowOption()
     {
