6491afd5d3c696b7445f97168a63cf10bd7804bb
yoon-H <ucando0320@gmail.com>
2022-09-23 14:30:02 +0900

    한윤재 화살타워 스탯 추가

--- PlumTowerDefence/Assets/Scripts/Tower/ArrowTower.cs
+++ PlumTowerDefence/Assets/Scripts/Tower/ArrowTower.cs
@@ -11,4 +11,45 @@ public class ArrowTower : Tower
     {
         Setstat(ETowerName.Arrow);
     }
+
+    public override float AttackStat
+    {
+        get
+        {
+            List<float> plus = TowerUpgradeAmount.instance._ArrowTowerStat.AttackPlusModifier;
+
+            float sum = 0f;
+
+            for (int i = 0; i <plus.Count; i++)
+            {
+                sum += plus[i];
+            }
+
+            return (BaseAttackStat + sum + AttackBuffAmount);
+
+        }
+        
+    }
+
+    public override float SpeedStat
+    {
+        get
+        {
+            List<float> plus = TowerUpgradeAmount.instance._ArrowTowerStat.SpeedPlusModifier;
+
+            float sum = 0f;
+
+            for (int i = 0; i < plus.Count; i++)
+            {
+                sum += plus[i];
+            }
+
+            return (BaseSpeedStat + sum + SpeedBuffAmount);
+
+        }
+    }
+
+    // 독 뎀 넣어주기
+
+    // 화상 데미지 넣어주기
 }
